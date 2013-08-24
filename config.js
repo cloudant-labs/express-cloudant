6a7cae77cafeba552ed061406695b48e179ee2ee
Max Thayer <garbados@gmail.com>
2013-08-24 18:56:19 -0400

    config exposes environment variables :O

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
-module.exports = {
-  "user": process.env.USERNAME || "USERNAME",
-  "pass": process.env.PASSWORD || "PASSWORD"
-}
+// map options to environment variables
+var options = process.env;
+
+// special settings
+options.USERNAME = options.USERNAME || "USERNAME";
+options.PASSWORD = options.PASSWORD || "PASSWORD";
+
+// expose to the application
+module.exports = options;
